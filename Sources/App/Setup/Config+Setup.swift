dd76d0038b96f42036143ca6d7f8cb20fbdd4ccd
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-03 14:23:36 +0100

    Remove all the unnecessary code and use 2 level indentation style

--- Sources/App/Setup/Config+Setup.swift
+++ Sources/App/Setup/Config+Setup.swift
@@ -1,23 +1,19 @@
 import FluentProvider
 
 extension Config {
-    public func setup() throws {
-        // allow fuzzy conversions for these types
-        // (add your own types here)
-        Node.fuzzy = [Row.self, JSON.self, Node.self]
-
-        try setupProviders()
-        try setupPreparations()
-    }
+  public func setup() throws {
+    Node.fuzzy = [Row.self, JSON.self, Node.self]
     
-    /// Configure providers
-    private func setupProviders() throws {
-        try addProvider(FluentProvider.Provider.self)
-    }
+    try setupProviders()
+    try setupPreparations()
+  }
+  
+  /// Configure providers
+  private func setupProviders() throws {
+    try addProvider(FluentProvider.Provider.self)
+  }
+  
+  private func setupPreparations() throws {
     
-    /// Add all models that should have their
-    /// schemas prepared before the app boots
-    private func setupPreparations() throws {
-        preparations.append(Post.self)
-    }
+  }
 }
