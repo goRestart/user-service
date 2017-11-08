ea5828e6cd8638fc33dcde8a3506a858cf95bab4
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-08 21:15:32 +0100

    Free token for u 🖖🏽

--- Package.swift
+++ Package.swift
@@ -2,23 +2,6 @@
 
 import PackageDescription
 
-#if os(Linux)
-  import Glibc
-#else
-  import Darwin
-#endif
-
-// MARK: - Helper to get environment variables from system
-
-enum Env {
-  static func get(_ name: String) -> String? {
-    guard let out = getenv(name) else { return nil }
-    return String(validatingUTF8: out)
-  }
-}
-
-let githubToken = Env.get("GITHUB_TOKEN")!
-
 let package = Package(
   name: "user-service",
   products: [
@@ -30,7 +13,7 @@ let package = Package(
     .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.3.0")),
     .package(url: "https://github.com/vapor-community/postgresql-provider.git", .upToNextMajor(from: "2.1.0")),
     .package(url: "https://github.com/vapor/validation-provider.git", .upToNextMajor(from: "1.2.0")),
-    .package(url: "https://\(githubToken):x-oauth-basic@github.com/skyweb07/core-service.git", .branch("develop"))
+    .package(url: "https://7c97062862d0720b759ca3f6bc253dc7c6730e9d:x-oauth-basic@github.com/skyweb07/core-service.git", .branch("develop"))
     ],
   targets: [
     .target(name: "App", dependencies: ["Vapor", "FluentProvider", "PostgreSQLProvider", "ValidationProvider", "CoreService"],
