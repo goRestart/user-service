26651c3e703de15007aab349582e798b279bdaf1
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-20 16:49:17 +0100

    Update project dependencies

--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
     .executable(name: "Run", targets: ["Run"])
   ],
   dependencies: [
-    .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.3.0")),
+    .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.4.0")),
     .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.3.0")),
     .package(url: "https://github.com/vapor-community/postgresql-provider.git", .upToNextMajor(from: "2.1.0")),
     .package(url: "https://github.com/vapor/validation-provider.git", .upToNextMajor(from: "1.2.0")),
-    .package(url: "https://7c97062862d0720b759ca3f6bc253dc7c6730e9d:x-oauth-basic@github.com/skyweb07/core-service.git", .branch("develop"))
+    .package(url: "https://github.com/goRestart/core-service.git", .branch("develop"))
     ],
   targets: [
     .target(name: "App", dependencies: ["Vapor", "FluentProvider", "PostgreSQLProvider", "ValidationProvider", "CoreService"],
