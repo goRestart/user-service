543f31efe7b19b7a2e9de4e9cfa598f1fe75a32a
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-20 16:38:22 +0100

    Check hash by using BCrypt instead of a simple equals

--- Sources/App/Data/Repository/Data Source/Disk/Task/CreatePasswordHashTask.swift
+++ Sources/App/Data/Repository/Data Source/Disk/Task/CreatePasswordHashTask.swift
@@ -10,7 +10,13 @@ struct CreatePasswordHashTask {
   }
   
   func create(with password: String, salt: String = UUID().uuidString) throws -> (hash: String, salt: String) {
-    let hash = try hasher.make(salt + password).makeString()
+    let hash = try hasher.make(
+      self.hash(for: salt, and: password)
+    ).makeString()
     return (hash: hash, salt: salt)
+  }
+  
+  func hash(for salt: String, and password: String) -> String {
+    return salt + password
   }
 }
